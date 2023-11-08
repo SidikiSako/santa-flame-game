351014d53a22749d1dd8fccfafda310f20532d17
Sidiki Sako <sidiksako@gmail.com>
2023-11-08 10:02:35 +0100

    use timer to create trees

--- lib/santa_game.dart
+++ lib/santa_game.dart
@@ -1,14 +1,29 @@
 import 'dart:async';
 
 import 'package:flame/game.dart';
+import 'package:flame/timer.dart';
 import 'package:santa_game/components/background.dart';
 import 'package:santa_game/components/tree.dart';
 
 class SantaGame extends FlameGame {
+  late Timer timer;
+
   @override
   FutureOr<void> onLoad() async {
     add(Background());
-    add(TreeComponent());
+    timer = Timer(
+      1.5,
+      repeat: true,
+      onTick: () {
+        add(TreeComponent());
+      },
+    );
     return super.onLoad();
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    timer.update(dt);
   }
 }
