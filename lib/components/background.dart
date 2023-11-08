778550c129b3549ce006433ad9d2fea230b1d734
Sidiki Sako <sidiksako@gmail.com>
2023-11-08 09:36:44 +0100

    add parallax effect to background

--- lib/components/background.dart
+++ lib/components/background.dart
@@ -1,11 +1,24 @@
 import 'dart:async';
 
 import 'package:flame/components.dart';
+import 'package:flame/parallax.dart';
+import 'package:santa_game/santa_game.dart';
 
-class Background extends SpriteComponent {
+class Background extends ParallaxComponent<SantaGame> {
   @override
   FutureOr<void> onLoad() async {
-    sprite = await Sprite.load('parallax/sky.png');
+    parallax = await game.loadParallax([
+      ParallaxImageData('parallax/sky.png'),
+      ParallaxImageData('parallax/bg.png'),
+      ParallaxImageData('parallax/middle.png'),
+      ParallaxImageData('parallax/foreground.png'),
+      ParallaxImageData('parallax/ground_01.png'),
+      ParallaxImageData('parallax/ground_02.png'),
+      ParallaxImageData('parallax/snow.png'),
+    ],
+        baseVelocity: Vector2(100, 0),
+        velocityMultiplierDelta: Vector2(1.1, 1.0));
+
     return super.onLoad();
   }
 }
